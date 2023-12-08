c3978e422892d6c86c3864436617429e31cf838d
heyimilya <ilya.shuvatov1@gmail.com>
2023-12-08 14:24:15 +0100

    before blog card

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import {Htag} from "@/components";
+import {Button} from "@/components";
+import {Paragraph} from "@/components";
+import {Tag} from "@/components";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-     
+    <main>
+      <Htag tag='h3'>Hello</Htag>
+      <Button appearance='primary' arrow='right'>Button</Button>
+      <Button appearance='ghost' arrow='down'>Button</Button>
+      <Paragraph size='large'>Large paragraph</Paragraph>
+      <Tag color='green' size='small'>Small Tag</Tag>
     </main>
-  )
+  );
 }
